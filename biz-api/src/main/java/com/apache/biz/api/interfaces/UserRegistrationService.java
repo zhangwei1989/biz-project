55b9a8b6bba2aca164766447cc4f9c69a377beeb
zhangwei <331874675@qq.com>
2024-01-30 16:52:02 +0800

    初始化父级项目及各子模块

---
+++ biz-api/src/main/java/com/apache/biz/api/interfaces/UserRegistrationService.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.apache.biz.api.interfaces;
+
+import com.apache.biz.api.model.User;
+import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+
+/**
+ * 用户注册服务接口（Open Feign、Dubbo 等公用）
+ * <p>
+ * JAX-RS
+ * Dubbo
+ * OpenFeign
+ * RMI -> 动态代理（基于接口编程）
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
+ * @since
+ */
+@FeignClient(name = "user-service")
+@DubboService
+public interface UserRegistrationService {
+
+    @PostMapping(value = "/user/register", produces = "application/json;v=3") // V3
+    Boolean registerUser(@RequestBody @Validated @Valid User user);
+
+}
---
+++ biz-api/src/main/java/com/apache/biz/api/model/User.java
@@ -0,0 +1,29 @@
+package com.apache.biz.api.model;
+
+import javax.validation.constraints.NotNull;
+
+public class User {
+
+    @NotNull
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
